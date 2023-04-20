e221803634a422ea66fd81f6f788b7571f55aa86
Aaron <aaturner1995@gmail.com>
2023-04-20 16:47:19 -0300

    connection to server created with sequelize. Models created for the User, blog and comments

--- models/Comment.js
+++ models/Comment.js
@@ -2,9 +2,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Blog extends Model {}
+class Comment extends Model {}
 
-Blog.init(
+Comment.init(
     {
         id: {
             type: DataTypes.INTEGER,
@@ -12,11 +12,7 @@ Blog.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        description: {
+        comment_body: {
             type: DataTypes.STRING,
         },
         date_created: {
@@ -31,12 +27,19 @@ Blog.init(
                 key: 'id',
             }
         },
+        blog_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'blog',
+                key: 'id',
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
-        modelName: 'blog'
+        modelName: 'comment'
     }
 );
 
-module.exports = Blog;
+module.exports = Comment;
