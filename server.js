ebfb7994469d9fe58b681836c53710edcdf49f53
Aaron <aaturner1995@gmail.com>
2023-04-20 15:48:06 -0300

    initial commit. Create the MVC model layout for files

---
+++ server.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+// Session store implementation for the express-session middleware in Node.js
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+// Crate instance of express handlebars with custom helpers passed to all templates
+const hbs = exphbs.create({ helpers });
+
+// Create the session and associate to the storage above
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+// Express middleware
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view enginer', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Now Listening on Port ${PORT}`))
+})
