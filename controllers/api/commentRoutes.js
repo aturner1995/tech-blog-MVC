808f0a0072b40e157c47fc8aa19bd803bdb610e8
Aaron <aaturner1995@gmail.com>
2023-04-22 22:14:28 -0300

    fixed styling on forms for UI and UX

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -6,24 +6,24 @@ router.get('/', async (req, res) => {
     try {
         const commentData = await Comment.findAll();
         res.status(200).json(commentData);
-    }
-    catch(err) {
+    } catch(err) {
         res.status(500).json(err);
     }
-})
+});
 
 router.post('/', async (req, res) => {
     try {
-        const commentData = await Comment.create({
-            blog_id: req.body.blog_id,
-            comment_description: req.body.description,
-            user_id: req.session.user_id,
-        });
-        res.status(200).json(commentData);
+      console.log(req.body.comment_description);
+      const commentData = await Comment.create({
+        blog_id: req.body.blog_id,
+        comment_description: req.body.comment_description,
+        user_id: req.session.user_id,
+      });
+      res.status(200).json(commentData);
+    } catch(err) {
+      res.status(500).json(err);
     }
-    catch(err) {
-        res.status(500).json(err);
-    }
-})
+  });
+  
 
 module.exports = router;
